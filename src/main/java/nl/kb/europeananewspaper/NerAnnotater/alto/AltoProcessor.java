abd5ae705ac48bda704d444be192f8e8fcb28749
Willem Jan Faber <willemjan.faber@kb.nl>
2013-12-12 16:26:15 +0100

    Made the layout more readable

--- src/main/java/nl/kb/europeananewspaper/NerAnnotater/alto/AltoProcessor.java
+++ src/main/java/nl/kb/europeananewspaper/NerAnnotater/alto/AltoProcessor.java
@@ -44,68 +44,69 @@ public class AltoProcessor {
     }
 
 
-	/**
-	 * @param potentialAltoFilename
-	 * @param mimeType
-	 * @param lang
-	 * @param handler
-	 * @throws IOException
-	 */
-	public static void handlePotentialAltoFile(final URL potentialAltoFilename,
-			final String mimeType, final Locale lang, final ResultHandler[] handler)
-			throws IOException {
-		if ("text/xml".equalsIgnoreCase(mimeType)
-				|| potentialAltoFilename.getFile().endsWith(".xml")) {
-			try {
-				System.out.println("Trying to process ALTO file "
-						+ potentialAltoFilename);
-				long startTime = System.currentTimeMillis();
-				Document doc = Jsoup.parse(potentialAltoFilename.openStream(),
-						null, "", Parser.xmlParser());
-				Elements elementsByTag = doc.getElementsByTag("alto");
-				if (elementsByTag.isEmpty()) {
-					System.err.println("Does not seem to be a ALTO file: "
-							+ potentialAltoFilename.toExternalForm());
-					return;
-				}
+    /**
+     * @param potentialAltoFilename
+     * @param mimeType
+     * @param lang
+     * @param handler
+     * @throws IOException
+     */
+    public static void handlePotentialAltoFile(final URL potentialAltoFilename,
+            final String mimeType, final Locale lang, final ResultHandler[] handler)
+            throws IOException {
+        if ("text/xml".equalsIgnoreCase(mimeType)
+                || potentialAltoFilename.getFile().endsWith(".xml")) {
+            try {
+                System.out.println("Trying to process ALTO file "
+                        + potentialAltoFilename);
+                long startTime = System.currentTimeMillis();
+                Document doc = Jsoup.parse(potentialAltoFilename.openStream(),
+                        null, "", Parser.xmlParser());
+                Elements elementsByTag = doc.getElementsByTag("alto");
+                if (elementsByTag.isEmpty()) {
+                    System.err.println("Does not seem to be a ALTO file: "
+                            + potentialAltoFilename.toExternalForm());
+                    return;
+                }
 
-				// we have an Alto file
-				for (ResultHandler h : handler) {
-					h.startDocument();
-					h.setAltoDocument(doc);
-				}
-				@SuppressWarnings("unchecked")
-				CRFClassifier<CoreMap> classifier_alto = (CRFClassifier<CoreMap>) NERClassifiers.getCRFClassifierForLanguage(lang);
-				CRFClassifier classifier_text = NERClassifiers.getCRFClassifierForLanguage(lang);
+                // we have an Alto file
+                for (ResultHandler h : handler) {
+                    h.startDocument();
+                    h.setAltoDocument(doc);
+                }
 
-				List<List<CoreMap>> coreMapElements = TextElementsExtractor.getCoreMapElements(doc);
-				int totalNumberOfWords = 0;
-				int classified = 0;
+                @SuppressWarnings("unchecked")
+                CRFClassifier<CoreMap> classifier_alto = (CRFClassifier<CoreMap>) NERClassifiers.getCRFClassifierForLanguage(lang);
+                CRFClassifier classifier_text = NERClassifiers.getCRFClassifierForLanguage(lang);
+                List<List<CoreMap>> coreMapElements = TextElementsExtractor.getCoreMapElements(doc);
+
+                int totalNumberOfWords = 0;
+                int classified = 0;
 
                 Map<String,String> awnser = new HashMap<String, String>();
 
-				for (List<CoreMap> block : coreMapElements) {
+                for (List<CoreMap> block : coreMapElements) {
                     int sentenceCount = 0;
                     int offset = 0;
-                    int totalNumberOfWords2 = 0;
+                    int offsetCount = 0;
 
-					for (ResultHandler h : handler) {
-						h.startTextBlock();
-					}
+                    for (ResultHandler h : handler) {
+                        h.startTextBlock();
+                    }
                     
                     // Loop over the alto to extract text elements. Make one long string (sentance).
-					List<CoreMap> classify_alto = classifier_alto.classify(block);
+                    List<CoreMap> classify_alto = classifier_alto.classify(block);
                     String text = "";
+
                     for (CoreMap label : classify_alto) {
                         if (label.get(HyphenatedLineBreak.class) == null) {
                             String word = cleanWord(label.get(OriginalContent.class));
-                            text = text +  word + " ";
+                            text = text + word + " ";
                             // label:
                             // [OriginalContent=Verhulst; TextAnnotation=Verhulst; AltoStringID=69:3233:45:880:29:677:3237:37:143 AnswerAnnotation=O]
                             // [OriginalContent=Rhap TextAnnotation=Rhapsodie AltoStringID=69:3233:45:880:33:850:3242:35:79 ContinuationAltoStringID=69:3274:43:878:1:69:3275:30:70 AnswerAnnotation=O]
                         }
                     }
-                
 
                     ArrayList<Map<String , String>> stanford_tokens  = new ArrayList<Map<String,String>>();
                     // Classify the output text, using the stanford tokenizer.
@@ -115,15 +116,14 @@ public class AltoProcessor {
                     // Loop over the stanford tokenzied words to map them to the alto later on.
                     for (List<CoreLabel> sentence : out) {
                         for (CoreLabel label: sentence) {
-						    if (label.get(HyphenatedLineBreak.class) == null) {
+                            if (label.get(HyphenatedLineBreak.class) == null) {
                                 StringTokenizer st = new StringTokenizer(label.get(OriginalTextAnnotation.class));
-
                                 // Sometimes the stanford tokenizer does not cut on whitespace (with numbers).
                                 while (st.hasMoreTokens()) {
                                     awnser = new HashMap<String, String>();
                                     awnser.put(st.nextToken(), label.get(AnswerAnnotation.class));
-                                    stanford_tokens.add(totalNumberOfWords2, awnser);
-                                    totalNumberOfWords2 += 1;
+                                    stanford_tokens.add(offsetCount, awnser);
+                                    offsetCount += 1;
                                 }
                                 // label :
                                 // [ValueAnnotation=Verhulst TextAnnotation=Verhulst OriginalTextAnnotation=Verhulst CharacterOffsetBeginAnnotation=260 
@@ -132,16 +132,16 @@ public class AltoProcessor {
                                 // PositionAnnotation=61 ShapeAnnotation=; GoldAnswerAnnotation=null AnswerAnnotation=O]
                                 // [ValueAnnotation=Rhap TextAnnotation=Rhap OriginalTextAnnotation=Rhap CharacterOffsetBeginAnnotation=270 CharacterOffsetEndAnnotation=274 BeforeAnnotation=  
                                 // PositionAnnotation=62 ShapeAnnotation=Xxxx GoldAnswerAnnotation=null AnswerAnnotation=O]
-                                                            }
+                            }
                         }
                     }
 
-					for (CoreMap label : classify_alto) {
-						if (label.get(HyphenatedLineBreak.class) != null) {
-							for (ResultHandler h : handler) {
-								h.newLine(label.get(HyphenatedLineBreak.class));
-							}
-						} else {
+                    for (CoreMap label : classify_alto) {
+                        if (label.get(HyphenatedLineBreak.class) != null) {
+                            for (ResultHandler h : handler) {
+                                h.newLine(label.get(HyphenatedLineBreak.class));
+                            }
+                        } else {
                             // label :
                             // [OriginalContent=Verhulst; TextAnnotation=Verhulst; AltoStringID=69:3233:45:880:29:677:3237:37:143 AnswerAnnotation=O]
                             // [OriginalContent=Rhap TextAnnotation=Rhapsodie AltoStringID=69:3233:45:880:33:850:3242:35:79 ContinuationAltoStringID=69:3274:43:878:1:69:3275:30:70 AnswerAnnotation=O]
@@ -179,12 +179,11 @@ public class AltoProcessor {
                             } else {
                                 offset -= 1;
                             }
-
                             // System.out.println(stanford_tokens);
                             // System.out.println(label.get(OriginalContent.class));
                             // System.out.println(stanford);
-							if (!label.get(AnswerAnnotation.class).equals("O")) {
-								classified += 1;
+                            if (!label.get(AnswerAnnotation.class).equals("O")) {
+                                classified += 1;
                                 for (ResultHandler h : handler) {
                                     try {
                                         h.addToken(
@@ -195,48 +194,49 @@ public class AltoProcessor {
                                                 label.get(ContinuationAltoStringID.class));
                                     } catch (Exception e) { };
                                 }
-							} else {
-								for (ResultHandler h : handler) {
-									h.addToken(
-											label.get(AltoStringID.class),
-											label.get(OriginalContent.class),
-											label.get(TextAnnotation.class),
-											null,
-											label.get(ContinuationAltoStringID.class));
-								}
-							}
-							totalNumberOfWords += 1;
+                            } else {
+                                for (ResultHandler h : handler) {
+                                    h.addToken(
+                                            label.get(AltoStringID.class),
+                                            label.get(OriginalContent.class),
+                                            label.get(TextAnnotation.class),
+                                            null,
+                                            label.get(ContinuationAltoStringID.class));
+                                }
+                            }
+                            totalNumberOfWords += 1;
                             sentenceCount += 1;
-						}
-					}
-					for (ResultHandler h : handler) {
-						h.stopTextBlock();
-					}
+                        }
+                    }
+                    for (ResultHandler h : handler) {
+                        h.stopTextBlock();
+                    }
 
-				}
+                }
 
-				for (ResultHandler h : handler) {
-					h.stopDocument();
-				}
-				System.out.println();
-				System.out.println("Statistics: "
-						+ classified
-						+ " out of "
-						+ totalNumberOfWords
-						+ "/ a " 
-						+ ((double) classified / (double) totalNumberOfWords) + ") classified");
-				System.out.println("Total millisecs: "
-						+ (System.currentTimeMillis() - startTime));
+                for (ResultHandler h : handler) {
+                    h.stopDocument();
+                }
 
-			} catch (IOException e) {
-				System.err.println("Could not read ALTO file "
-						+ potentialAltoFilename.toExternalForm());
-				throw e;
-			} finally {
-				for (ResultHandler h : handler) {
-					h.close();
-				}
-        	}
-		}
-	}
+                System.out.println();
+                System.out.println("Statistics: "
+                        + classified
+                        + " out of "
+                        + totalNumberOfWords
+                        + "/ a " 
+                        + ((double) classified / (double) totalNumberOfWords) + ") classified");
+                System.out.println("Total millisecs: "
+                        + (System.currentTimeMillis() - startTime));
+
+            } catch (IOException e) {
+                System.err.println("Could not read ALTO file "
+                        + potentialAltoFilename.toExternalForm());
+                throw e;
+            } finally {
+                for (ResultHandler h : handler) {
+                    h.close();
+                }
+            }
+        }
+    }
 }
