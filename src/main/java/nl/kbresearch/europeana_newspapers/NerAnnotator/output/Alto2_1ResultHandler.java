ee3b6b7d81f8ef4e81022d1a4967782ed95df158
Willem Jan Faber <WillemJan.Faber@kb.nl>
2014-02-25 16:04:40 +0100

    Fix for Encoding issue #7

--- src/main/java/nl/kbresearch/europeana_newspapers/NerAnnotator/output/Alto2_1ResultHandler.java
+++ src/main/java/nl/kbresearch/europeana_newspapers/NerAnnotator/output/Alto2_1ResultHandler.java
@@ -186,6 +186,9 @@ public class Alto2_1ResultHandler implements ResultHandler {
             // Reformat output, because of additional nodes added.
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            // Set the right output encoding
+            transformer.setOutputProperty("encoding", "ISO-8859-1");
+            // Transform the input document to output
             transformer.transform(domSource, result);
 
             // Store results to file.
